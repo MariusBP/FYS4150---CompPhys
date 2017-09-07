6c412f97647b41835386635944fe870a20f2757c
Marius B. Pran <MariusBPran@gmail.com>
2017-09-07 12:41:09 +0200

    hei

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,45 +3,53 @@
 #include <armadillo>
 #include <vector>
 #include <ctime>
+#include <cstdio>
+
 using namespace std;
 using namespace arma;
 
 int main()
   {
-    clock_t start;
+    int start = clock();
     int n;
-    cout << "Give me your valuables! *Points gun*" << endl;
+    cout << "Give me n valuables! *Points gun*" << endl;
     cin >> n;
     //initialisation
-    vec d(n+1);
-    vec e(n);
+    vec d = randu(n+1);
+    vec e = randu(n+1);
     vec dt(n+1);
     vec ft(n+1);
-    vec u(n+1);
-    u(0) = 0;
-    u[n] = 0;
+    vec v(n+1);
+    v(0) = 0;
+    v[n] = 0;
     vec x;
     x = linspace(0,1,n+1);
     vec f;
     double h = (x[n]-x[0])/n;
     f = h*h*100*exp(-10*x);
+    vec sol = 1 - (1 - exp(-10))*x - exp(-10*x);
 
-    d.fill(2.0);
-    e.fill(-1.0);
     dt[0] = d[0];
     ft[0] = f[0];
 
+    //forwards substitution
     for(int i = 1; i<n+1; i++){
         dt[i] = d[i] - (e[i-1]*e[i-1])/dt[i-1];
         ft[i] = f[i] - e[i-1]*ft[i-1]/dt[i-1];
     }
 
-    u[n-1] = ft[n-1]/dt[n-1];
+    v[n-1] = ft[n-1]/dt[n-1];
 
+    //backwards solution
     for(int i=n-1; i>0; i--){
-        u[i] = (ft[i] - e[i]*u[i+1])/dt[i];
+        v[i] = (ft[i] - e[i]*v[i+1])/dt[i];
     }
-    cout << (clock()-start)/float(CLOCKS_PER_SEC) << endl;
-    for(int i = 0; i<n+1; i++)
-    cout << u[i] << "\t" << 1 - (1 - exp(-10))*x[i] - exp(-10*x[i])<< endl;
+    ofstream myfile;
+    string name = "values" + to_string(n) + ".txt";
+    myfile.open (name);
+    for(int i = 0; i<n+1; i++){
+        myfile << v[i] << "\t" <<sol[i] <<"\n";
+    }
+    myfile.close();
+    cout << "Program run time: "<<(clock()-start)/float(CLOCKS_PER_SEC)<< " seconds" << endl;
 }
