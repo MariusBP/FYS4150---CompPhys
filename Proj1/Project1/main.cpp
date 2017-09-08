db8ee0378ea26b90559ea104f012e15856e18f6d
Marius B. Pran <MariusBPran@gmail.com>
2017-09-08 12:29:53 +0200

    works?

---
+++ Proj1/Project1/main.cpp
@@ -0,0 +1,64 @@
+//Project 1
+#include <iostream>
+#include <armadillo>
+#include <vector>
+#include <ctime>
+#include <cstdio>
+
+using namespace std;
+using namespace arma;
+
+int main()
+  {
+    int n;
+    cout << "Give me n valuables! *Points gun*" << endl;
+    cin >> n;
+    //initialisation
+    int start = clock();
+    vec d(n+1);
+    d.fill(2);    
+    vec e(n+1);
+    e.fill(-1);
+    vec dt(n+1);
+    vec ft(n+1);
+    vec v(n+1);
+    v(0) = 0;
+    v[n] = 0;
+    vec x;
+    x = linspace(0,1,n+1);
+    vec f;
+    double h = (x[n]-x[0])/n;
+    f = h*h*100*exp(-10*x);
+    vec u = 1 - (1 - exp(-10))*x - exp(-10*x);
+
+    dt[0] = d[0];
+    ft[0] = f[0];
+
+    //forwards substitution
+    for(int i = 1; i<n+1; i++){
+        dt[i] = d[i] - (e[i-1]*e[i-1])/dt[i-1];
+        ft[i] = f[i] - e[i-1]*ft[i-1]/dt[i-1];
+    }
+
+    v[n-1] = ft[n-1]/dt[n-1];
+
+    //backwards substitution
+    for(int i=n; i>0; i--){
+        v[i] = (ft[i] - e[i]*v[i+1])/dt[i];
+    }
+
+    ofstream myfile;
+    string name = "values" + to_string(n) + ".txt";
+    myfile.open (name);
+    for(int i = 0; i<n+1; i++){
+        myfile << x[i] <<"\t" << v[i] << "\t" <<u[i] <<"\n";
+    }
+    myfile.close();
+
+    vec err = log10(abs((v-u)/u));
+    err(0) = 0;
+    err(n) = 0;
+    cout << "The error is: "<< err.max()<<endl;
+    cout << "Program run time: "<<(clock()-start)/float(CLOCKS_PER_SEC)<< " seconds" << endl;
+
+}
---
+++ Proj1/Project1/plot.py
@@ -0,0 +1,35 @@
+from matplotlib.pyplot import *
+from numpy import *
+
+A = genfromtxt("values10.txt")
+B = genfromtxt("values100.txt")
+C = genfromtxt("values1000.txt")
+
+plot(A[:, 0], A[:, 1], A[:,0], A[:, 2])
+grid('on')
+title('n = 10')
+xlabel('x')
+ylabel('u')
+legend(['Computed', 'Exact'])
+savefig("values10")
+close()
+
+plot(B[:, 0], B[:, 1], B[:,0], B[:, 2])
+grid('on')
+title('n = 100')
+xlabel('x')
+ylabel('u')
+legend(['Computed', 'Exact'])
+savefig("values100")
+close()
+
+plot(C[:, 0], C[:, 1], C[:,0], C[:, 2])
+grid('on')
+title('n = 1000')
+xlabel('x')
+ylabel('u')
+legend(['Computed', 'Exact'])
+savefig("values1000")
+close()
+
+#ho
